0e0a739194c504c006fc14618be7125d139cb9b5
sah-rohit <rohit2006gamer@gmail.com>
2026-04-27 14:21:11 +0530

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ieoodf9lbs"
+    "revision": "0.rr1setqkcbs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
